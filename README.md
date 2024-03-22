1d8af7de78e19344e33909020656f425a4cafad3
VictorHugoPilled <echase3@gmail.com>
2024-03-22 04:55:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Setup Steps
+
+## CSV Setup
+1. Place the day, the script time and script path in the scheduler.csv. There are examples in the csv for you to follow.
+   - **Don't remove the heading**, it is ignored when the script is ran, but if you do remove it, all of your scripts will not run.
+   - Don't worry about the order you put the scripts in, they will be sorted in the code.
+   - Days must be capitalized: Monday, Tuesday, etc.
+   - Time must be written in the 24 hour format.
+
+## Workflow setup
+1. Open scheduler.workflow with automator.
+2. Replace the following with whatever directory you decide to leave this folder. 
+
+```Shell
+cd $HOME/scheduler
+```
+	- example:
+	```Shell
+	cd /Users/earlchase/documents/scheduling
+	```
+
+3. The script is exectuable and will run when called by the workflow so no further configuration is needed.
+
+## Automator App Setup
+1. Open automator.
+2. Click on open an existing document and open scheduler.app
+3. Confirm scheduler.workflow is selected as the workflow this app runs when opened.
+   - You shouldn't have to change anything at all to get to work. All you are doing is confirming everything is in order.
+
+## Calendar
+1. Open your calendar and create a recurring event.
+   - The first time you should set this event to run basically immediately as the calendar is going to ask you for permission to open the automator app.
+   - Once you have run this one time and given the permission to run, change the date of the event to about an hour before you normally run your scripts.
+2. Click on alert and choose customize.
+3. Then click on the top dropdown menu that shows up and choose open a file.
+4. Click on calendar and select other.
+5. Select the scheduler.app
+6. Click ok and you are done.
+
+## Wrapup
+- Don't worry about making your scripts executable, all scripts are ran like this:
+
+```Shell
+python3 /Users/earlchase/documents/scheduling.py
+```
+
+- If you need to run more than one script per day, this script will run them in order. 
---
+++ scheduler.sh
@@ -0,0 +1,128 @@
+#!/bin/zsh
+
+
+# get todays scripts from a csv that must be passed
+# to this script as the first argument
+function parseCSV(){
+
+    # csv variable
+    csv=$1
+
+    # if the csv doesn't exist, exit the function
+    if [[ ! -f "$csv" ]]; then
+	echo "File not found"
+	return
+    fi
+
+    # create filelength integer variable so we don't get the header
+    typeset -i fileLength=$(< $csv | wc -l)
+
+    # read in the file, sort based on the times and ignore the header
+    scripts=$( < $csv | sort -n | tail -n $fileLength )
+
+    # return the scripts read in from the csv
+    echo $scripts
+
+}
+
+
+# pass the scheduler as the first argument
+function getTodaysScripts(){
+
+    # %A = locale's full weekday name (e.g., Sunday)
+    day=$(date "+%A")
+    
+    # store the csv variable
+    csv=$1
+
+    # parse the csv config file
+    scripts=$(parseCSV $csv)
+
+    # if there are no scripts today do nothing
+    if [[ ! -n scripts ]]; then
+	echo "No scripts today"
+	return
+    fi
+
+    # create associate array for scripts
+    typeset -A todaysScripts
+
+    # iterate through each script
+    while read -r lines;do
+	scriptDay=$(echo $lines | awk -F"," '{print $1}')
+	if [[ $scriptDay == $day ]]; then
+	    # read in scriptname and time
+	    scriptName=$(echo $lines | awk -F"," '{print $3}')
+	    scriptTime=$(echo $lines | awk -F"," '{print $2}')
+	    # place them in the associative array
+	    todaysScripts[$scriptName]=$scriptTime
+	fi
+    # pipe the scripts variable into the above loop
+    done <<< $scripts
+
+    # return the associative array of todays scripts
+    echo ${(kv)todaysScripts}
+    
+}
+
+
+# first argument is scriptname
+# second argument is the time you would like to schedule the script
+function schedule() {
+    
+    # Get script time
+    scriptTime=$2
+
+    # Wait until its time to run
+    # %R = 24-hour hour and minute
+    while [[ $(date "+%R") != $scriptTime ]]; do
+	sleep 1
+    done
+    
+    # Check if the file exists before running it
+    scriptName=$1
+    if [[ -f "$scriptName" ]];then
+	# run and send to background
+	python3 $scriptName &
+    fi
+    
+}
+
+# takes csv with files as first argument
+# and runs every script that needs to be run today
+function main(){
+
+    # store the csv
+    csv=$1
+    
+    # create an associate array for todays scripts
+    typeset -A todaysScripts
+
+    # get a copy of today's associative array
+    todaysScripts=( $(getTodaysScripts $csv) )
+
+    # if the variable is empty exit the script
+    if [[ ! -n $todaysScripts  ]];then
+	echo "No scripts to run"
+	return
+    fi
+    
+    for scriptName scriptTime in ${(kv)todaysScripts};do
+	echo "scheduling ${scriptName} to run at ${scriptTime}"
+
+	# schedule each script
+	schedule ${scriptName} ${scriptTime}
+    done
+
+    # wait for all scripts to run before exiting
+    wait
+        
+}
+
+# if you need to change the name of the csv, do it here
+main scheduler.csv
+
+
+
+
+
