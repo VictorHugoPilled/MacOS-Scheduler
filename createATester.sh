e5ef49c2c64c0a450c85d24a5893a33df323217a
VictorHugoPilled <echase3@gmail.com>
2024-03-29 07:16:46 -0400

    Done

--- createATester.sh
+++ createATester.sh
@@ -3,24 +3,31 @@ function testcsvcreator() {
     rm -rf testschedule.csv
 
     # delete previous files created by tests
-
-    rm -rf test.json
     rm -rf ~/testfile.txt
+    rm -rf test.json
+    
   
-    # store for heredoc
+    # store variables for heredoc
     testcsv="testschedule.csv"
     # python test files
-    pyfile1="createFile.py"
-    pyfile2="basicdownload.py"
-    pyfile3="~/thirdkey.py"
+    pyfile1='createFile.py'
+    pyfile2='basicdownload.py'
+    pyfile3=~/thirdkey.py
+
+    # Create entries that are 2 minutes, 3 minutes and 5 minutes from now
+    twominutes=$(date -j -f "%a %b %d %T %Z %Y" "`date -v+2M`" "+%H:%M")
+    threeminutes=$(date -j -f "%a %b %d %T %Z %Y" "`date -v+3M`" "+%H:%M")
+    fiveminutes=$(date -j -f "%a %b %d %T %Z %Y" "`date -v+5M`" "+%H:%M")
     
     # create three entries for today using heredoc
     cat <<EOF > $testcsv
-$(date +%A),$(date +%R),$pyfile1
-$(date +%A),$(date +%R),$pyfile2
-$(date +%A),$(date +%R),$pyfile3
+Day,Time,ScriptPath
+$(date +%A),$twominutes,$pyfile1
+$(date +%A),$threeminutes,$pyfile2
+$(date +%A),$fiveminutes,$pyfile3
 EOF
 
     echo "Done"
     
 }
+
--- scheduler.sh
+++ scheduler.sh
@@ -1,72 +1,36 @@
 #!/bin/zsh
 
 
-# get todays scripts from a csv that must be passed
-# to this script as the first argument
-function parseCSV(){
-
-    # csv variable
-    csv=$1
-
-    # if the csv doesn't exist, exit the function
-    if [[ ! -f "$csv" ]]; then
-	echo "File not found"
-	return
-    fi
-
-    # create filelength integer variable so we don't get the header
-    typeset -i fileLength=$(< $csv | wc -l)
-
-    # read in the file and ignore the header
-    scripts=$( < $csv | tail -n $fileLength )
-
-    # return the scripts read in from the csv
-    echo $scripts
-
-}
-
-
-# pass the scheduler as the first argument
+# pass the csv as the first argument
 function getTodaysScripts(){
     
     # store the csv variable
     csv=$1
 
-    # parse the csv config file
-    scripts=$(parseCSV "$csv")
-
-    # if there are no scripts, today do nothing
-    if [[ ! -n "$scripts" ]]; then
-	echo "No scripts today"
+    # if the csv doesn't exist, exit the function
+    if [[ ! -f "$csv" ]]; then
+	echo 'File not found'
 	return
     fi
 
-    # %A = locale's full weekday name (e.g., Sunday)
-    day=$(date "+%A")
-
-    # create associate array for scripts
-    typeset -A todaysScripts
-
-    # IFS = input field seperator
-    # IFS is set to an empty value
-    # each line from the file is assigned individually to $line
-    # -r = consider each backslash to be part of the input line
-    while IFS= read -r line;do
-	# s:string: = force field splitting at the separator string
-	# expand the line into an array
-	typeset -a array
-	array=("${(@s:,:)line}")
-	# array indices start at 1
-	if [[ ${array[1]} == $day ]]; then
-	    scriptName=${array[3]}
-	    scriptTime=${array[2]}
-	    # place them in the associative array
-	    todaysScripts[$scriptName]=$scriptTime
-	fi
-    done <<< $scripts
+    # get current day
+    currentday=$(date "+%A")
     
-    # return the associative array of todays scripts
-    echo ${(kv)todaysScripts}
+    # use sqlite3 to read in the csv
+    # select only today's scripts
+    # and order by time
+    scripts=$(sqlite3 :memory: <<EOF
+.mode csv
+.import $csv scheduler
+SELECT Time,ScriptPath FROM scheduler
+WHERE Day = '$currentday'
+ORDER BY Time;
+EOF
+ )
+    
+    
+    # return the scripts
+    echo ${scripts}
     
 }
 
@@ -74,59 +38,88 @@ function getTodaysScripts(){
 
 # first argument is scriptname
 # second argument is the time you would like to schedule the script
-function schedule() {
+function runner() {
     
     # Get script time
     scriptTime=$2
 
+    if [[ ! -n "$scriptTime" ]];then
+	echo 'A time to run is needed'
+	return
+    fi
+
+    # Check if the file exists before running it
+    scriptName=$1
+    if [[ ! -f "$scriptName" ]]; then
+	echo 'File does not exist'
+	return
+    fi
+
+    
     # Wait until its time to run
     # %R = 24-hour hour and minute
     while [[ $(date "+%R") != $scriptTime ]]; do
 	sleep 1
     done
     
-    # Check if the file exists before running it
-    scriptName=$1
-    [[ -f "$scriptName" ]] && { python3 "$scriptName" & }  || echo "File does not exists"
     
+    # run script in the background
+    python3 "$scriptName" &
+
 }
 
 
 
-# takes csv with files as first argument
+# takes csv as first argument
 # and runs every script that needs to be run today
 function main(){
 
     # store the csv
     csv=$1
-    
-    # create an associate array for todays scripts
-    typeset -A todaysScripts
 
-    # get a copy of today's associative array
-    todaysScripts=( $(getTodaysScripts $csv) )
+    # if the variable is empty exit the script
+    if [[ ! -n "$csv" ]];then
+	echo 'CSV needed'
+	return
+    fi
+
+    # get todays scripts from the csv
+    todaysScripts=$(getTodaysScripts $csv)
 
     # if the variable is empty exit the script
     if [[ ! -n "$todaysScripts"  ]];then
-	echo "No scripts to run"
+	echo 'No scripts to run'
 	return
     fi
-    
-    for scriptName scriptTime in ${(kv)todaysScripts};do
-	echo "scheduling ${scriptName} to run at ${scriptTime}"
 
-	# schedule each script
-	schedule ${scriptName} ${scriptTime}
-    done
+    echo $
 
-    # wait for all scripts to run before exiting
+    # read in the scripts line by line
+    while IFS= read -r line;do
+
+	# split the line into an array
+	typeset -a array=(${(@s:,:)line})
+
+	# scriptTime is first item in the array
+	scriptTime=${array[1]}
+	# scriptName is the second item
+	scriptName=${array[2]}
+
+	echo "${scriptName} will run at ${scriptTime}"
+
+	# send the script to the runner
+	# and store the pid
+	typeset -i pid=$(runner ${scriptName} ${scriptTime})
+	
+    done <<< $todaysScripts
+
     wait
+    
+
+
+    
 
 }
-
-
-
-
 
 
 
